bf17e3f1f7093dfce9716422187f30a41f93befb
Valerie Fuchs <work@valerie-fuchs.com>
2025-06-13 13:46:13 +0200

    Initial scaffold

---
+++ src/components/RoboticCrane/index.tsx
@@ -0,0 +1,3 @@
+export default function RoboticCrane() {
+    return <div>RoboticCrane</div>;
+} 
---
+++ src/components/UIControls/index.tsx
@@ -0,0 +1,11 @@
+import Inputs from "./Inputs";
+import Terminal from "./Terminal";
+
+export default function UIControls() {
+    return (
+        <div>
+            <Inputs />
+            <Terminal />
+        </div>
+    )
+} 
